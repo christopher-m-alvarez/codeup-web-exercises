a3fa552606a22ba21ac06681fa26306df06b25d7
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-06-04 15:45:41 -0500

    for loops

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,6 +1,8 @@
+'use strict';
 
-
-
+// if its not an odd number 1st if
+// if it is the prompt number then run just 2nd if
+// it is an odd number but not the prompt then 3rd if
 
 var oddNumber;
 
@@ -9,10 +11,12 @@ while(true) {
     if (oddNumber % 2 != 0) {
         break;
     }
-}
+} console.log("Number to skip is: " + oddNumber)
+
 
 for (let index = 0; index < 50; index++) {
     if (index == oddNumber) {
+        console.log("Yikes! Skipping number " + index);
         continue
     }
     if (index % 2 !=0) {
