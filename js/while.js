d1c16f8d6570c222cb03bb6af3f16cf785718266
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-06-04 17:06:34 -0500

    for loops

--- js/while.js
+++ js/while.js
@@ -7,7 +7,17 @@ var number = number * 2;
 
 while (true) {
     number++;
-if (number > 6700) {
+if (number > 67000) {
     break;
 }console.log()
 }
+
+//var number=0;
+//
+//while (true) {
+//    alert("Here!");
+//    number++;
+//    if (number > 2) {
+//        break;
+//    }
+//}
