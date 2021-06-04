0387ca4bf78b3c20beb7f2896e2c05d93637aae1
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-06-04 10:03:04 -0500

    for loops

---
+++ js/loop_classwork.js
@@ -0,0 +1,23 @@
+
+/* alert("herro")
+
+ */
+var message = "";
+
+// var howmany = prompt("How many?");
+
+//for (let index = 0; index <= howmany; index++){
+//    message = message + "index is: " + index + "\n";
+//}
+
+//for (let index = 10; index > 0; index = index - 4){
+//   message = message + "index is: " + index + "\n";
+//}
+
+//for (let index = 0; index <= 10; index++){
+//    message = message + "index is: " + index + "\n";
+//}
+
+alert(message);
+
+alert(index);
