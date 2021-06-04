c47669ea810def23c15dbcaa4facf473d4037922
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-06-04 12:27:04 -0500

    for loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -8,17 +8,43 @@
 //
 //alert(showMultiplicationTable(7))
 
-function showMultiplicationTable() {
+//function showMultiplicationTable() {
+//
+//    for (let Num = 0; Num < 10; Num++) {
+//        var ranNum = Math.ceil(Math.random() * (200 - 20) + 20);
+//        if (ranNum % 2 === 0)
+//
+//        {
+//            console.log(" This number is even" + ranNum)
+//        } else {
+//            console.log(" This number is odd" + ranNum)
+//        }
+//    }
+//}
+//showMultiplicationTable();
 
-    for (let Num = 0; Num < 10; Num++) {
-        var ranNum = Math.ceil(Math.random() * 100) + 10;
-        if (ranNum % 2 === 0)
+//#4 short way
 
-        {
-            console.log(" This number is even" + ranNum)
-        } else {
-            console.log(" This number is odd" + ranNum)
-        }
-    }
+//for (var index = 1,number = "";index < 10;index++) {
+//    number += "1";
+//    console.log(index * number)
+//}
+
+
+//function tree(){
+//    for (let x=1; x<10; x++){
+//        var number ="";
+//        number=x;
+//        for(let y = 1; y<x; y++){
+//            number = number+x.toString();
+//        }
+//        console.log(number.toString());
+//    }
+//}
+//tree()
+
+var message = "";
+for (let index = 100; index >= 5; index = index - 5){
+    message = message + "index is: " + index + "\n";
 }
-showMultiplicationTable();
+alert(message)
