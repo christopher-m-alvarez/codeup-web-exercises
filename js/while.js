7fe431075a2d32aafdb71335b0725dec85baf8b9
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-06-04 23:23:20 -0500

    for loops

--- js/while.js
+++ js/while.js
@@ -1,16 +1,23 @@
 
 
+let value = 2;
 
+while (value < 65537) {
+    console.log(value);
+    value = value * 2;
 
-var number = number * 2;
-
-
-while (true) {
-    number++;
-if (number > 67000) {
-    break;
-}console.log()
 }
+
+
+//where i got to wrong
+//var number = number * 2;
+
+//while (true) {
+//    number++;
+//if (number < 67000) {
+//    break;
+//}console.log()
+//}
 
 //var number=0;
 //
