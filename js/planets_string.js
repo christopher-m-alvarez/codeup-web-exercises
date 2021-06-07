d3168a64ef2d3cc5f3f853488d28388445c44058
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-06-07 15:41:39 -0500

    string exercises

--- js/planets_string.js
+++ js/planets_string.js
@@ -5,16 +5,18 @@
     var planetsArray;
 
     /**
-     * TODO:
+     *
      * Convert planetsString to an array, and save it in a variable named
      * planetsArray.
      * console.log planetsArray to check your work
      */
 
-    // console.log(planetsArray);
+
+     var planetsArray = planetsString.split("|");
+     console.log(planetsArray);
 
     /**
-     * TODO:
+     *
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
      *
@@ -23,5 +25,8 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+    var planetsString = planetsArray.join(" <br> ");
+        console.log(planetsString)
 
 })();
