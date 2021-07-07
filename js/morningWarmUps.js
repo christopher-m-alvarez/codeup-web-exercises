7bf543f2d0ee6432f5064a5f4830de0215ff23ac
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-07-07 09:24:48 -0500

    warm ups

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -42,3 +42,60 @@ describe('lowerCase', function () {
 //  each string being capitalized while having the rest of the string lowercase look at the example below for reference.
 // Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
 
+// July 7th 2021
+// TODO: Create a method on the object down below named getFullName that when called returns the users full name using the "this" key term.
+// Example: personOne.getFullName() ---- returns ----> "silvia floopertan"
+var personOne = {
+    firstName: "silvia",
+    lastName: "floopertan",
+    ageInYears: 34,
+    heightInInches: 65
+}
+
+personOne.getFullName = function(){
+    return personOne.firstName + " " + personOne.lastName
+}; console.log(personOne.sayHello());
+
+
+// TODO: Create a method on the object down below named getNicelyFormattedFullName that when called returns the
+//  users full name with both first letters in the names being capitalized look at example down below for more reference.
+// Example: personOne.getNicelyFormattedFullName() ---- returns ----> "Dan Valdarez"
+var personTwo = {
+    firstName: "dan",
+    lastName: "valdarez",
+    ageInYears: 61,
+    heightInInches: 73
+}
+
+personTwo.getNicelyFormattedFullName(){
+
+}
+
+
+// TODO: Create a method on the object below that gives us the behavior that the example implies.
+// Example: personThree.intro() ---- returns ----> "Hello, My name is Arlene Martinez and I am 25 years old"
+var personThree = {
+    firstName: "arlene",
+    lastName: "martinez",
+    ageInYears: 25,
+    heightInInches: 62
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
