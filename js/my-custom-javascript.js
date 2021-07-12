a14cd8ffebc6e4dc8d01eb73ed93529895a4af23
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-07-12 12:11:59 -0500

    mouse events exercise done

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -16,3 +16,21 @@
 
 // //Multiple Selectors
 // $('h1, p, li').css('background-color', 'red');
+
+
+$('h1').click(function() {
+    $(this).css('background-color', '#FF0')
+});
+
+$('p').dblclick(function() {
+    $(this).css('font-size', '18px')
+});
+
+$('li').hover(
+    function() {
+        $(this).css('color', 'red');
+    },
+    function() {
+        $(this).css('color', 'black');
+    }
+);
