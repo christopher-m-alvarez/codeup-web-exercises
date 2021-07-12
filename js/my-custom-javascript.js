7c3c9ebc573e76455e4524b601d5db288d7a4d27
Christopher Alvarez <christopher.m.alvarez93@gmail.com>
2021-07-12 10:19:27 -0500

    jquery exercise done

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,8 +1,18 @@
 "use strict";
 
+// //ID Selectors alerting contents
+// var contents = $("#list").html();
+// alert(contents);
 
-$(document).ready(function() {
 
-    alert( 'The DOM has finished loading!' );
+// //Class Selector using css in java
+// $('.codeup').css('border', '1px red solid');
 
-});
+// // Element Selector
+// $('li').css('font-size', '20px');
+// $('.something').css('background-color', 'red');
+// var contents = $("h1").html();
+// alert(contents);
+
+//Multiple Selectors
+$('.something, li').css('background-color', 'red');
